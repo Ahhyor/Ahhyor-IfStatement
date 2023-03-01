8ff57bdb7bf7c386fe53986f1ec9b653a8602668
ayobami adefarakan <ade192@live.com>
2023-03-01 22:51:46 +0000

    complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -20,6 +20,12 @@ public class Lab {
      * @return x if bool is true. if bool is false, return y.
      */
     public int ifExample(boolean bool, int x, int y){
+
+    if (x==y) {
+        return 1;
+    }
+    else {
         return 0;
+    }
     }
 }
